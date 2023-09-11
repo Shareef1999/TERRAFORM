7abe431ac595507f160bf5b9cd6d95de51e6eea9
Shareef1999 <mdshareefs143@gmail.com>
2023-09-11 22:45:51 +0530

    adding dynamic blocks code

---
+++ dynamic_blocks/variables.tf
@@ -0,0 +1,11 @@
+
+variable "sg_ports" {
+  type        = list(number)
+  description = "list of ingress ports"
+  default     = [8200, 8201,8300, 9200, 9500]
+}
+
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+}
